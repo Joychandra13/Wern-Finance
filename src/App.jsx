7ebd9190cd11ba1d07800ad8519fe481dea6dfe0
Joychandra13 <joychandra1313@gmail.com>
2024-11-25 18:34:13 +0600

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Vision from './components/Vision'
 function App() {
   return (
     <>
-      <main className="overflow-x-hidden bg-[#1a1e1c] font-mplus1 text-white bg-[url(bg.svg)] bg-no-repeat bg-cover">
+      <main className="overflow-x-hidden">
         <NevBar/>
           <div className="w-full max-w-[1440px] px-2.5 md:px-10 lg:px-20 xl:px-24 mx-auto">
             <div className="space-y-[128px]">
